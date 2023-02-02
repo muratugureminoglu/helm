7ebf2005201bca2a751bb7d56403ae2c8e676b56
Murat Ugur Eminoglu <murat@murat.ws>
2023-02-02 14:34:31 +0300

    Update package.sh

--- package.sh
+++ package.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-helm uninstall antmedia -n antmedia
-#rm *.tgz
 helm dependency update
 helm package $(pwd)/.
 helm repo index --url https://ant-media.github.io/helm/ --merge index.yaml .
+cat $(pwd)/Chart.yaml |grep "version: [0-9].[0-9]" | cut -d " " -f 2 > $(pwd)/VERSION

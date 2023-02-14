09b1c38887cd6349f5f41a83810b5a3cb3698b03
murat <murat@murat.ws>
2023-02-14 11:38:16 +0300

    Delete all files

--- package.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-rm $(pwd)/antmedia-$(cat VERSION).tgz
-helm dependency update
-helm package $(pwd)/.
-helm repo index --url https://ant-media.github.io/helm/ --merge index.yaml .
-cat $(pwd)/Chart.yaml |grep "version: [0-9].[0-9]" | cut -d " " -f 2 > $(pwd)/VERSION

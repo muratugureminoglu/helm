08ab352084b386de3da15535a038828123c3264b
Murat Ugur Eminoglu <murat@murat.ws>
2023-06-02 12:17:30 +0300

    Update package.sh

--- package.sh
+++ package.sh
@@ -4,3 +4,4 @@ helm dependency update
 helm package $(pwd)/.
 helm repo index --url https://ant-media.github.io/helm/ --merge index.yaml .
 cat $(pwd)/Chart.yaml |grep "version: [0-9].[0-9]" | cut -d " " -f 2 > $(pwd)/VERSION
+
